64f67a274b8d238ee355fe87b7df18ac15650abf
siejie <siejiehie@foxmail.com>
2017-02-21 09:37:14 +0800

    common annotation

---
+++ src/main/java/com/wide/hello/DemoObj.java
@@ -0,0 +1,35 @@
+package com.wide.hello;
+
+/**
+ * Created by Sai on 2017/2/20.
+ */
+public class DemoObj {
+
+    private Long id;
+    private String name;
+
+    public DemoObj() {
+    }
+
+    public DemoObj(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/wide/hello/DemoRestController.java
@@ -0,0 +1,23 @@
+package com.wide.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Sai on 2017/2/21.
+ */
+@RestController
+@RequestMapping("/rest")
+public class DemoRestController {
+
+    @RequestMapping(value = "/getjson", produces = "application/json;charset=UTF-8")
+    public DemoObj getJson(DemoObj obj) {
+        return new DemoObj(obj.getId() + 1, obj.getName() + "-json");
+    }
+
+    @RequestMapping(value = "/getxml", produces = "application/xml;charset=UTF-8")
+    public DemoObj getXml(DemoObj obj) {
+        return new DemoObj(obj.getId() + 1, obj.getName() + "-xml");
+    }
+
+}
